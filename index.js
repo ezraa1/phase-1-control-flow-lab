fe7c3234feeba07b9c1b76b826339d57542987f3
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-06-07 22:58:18 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+
   // Write your code here!
+  function scuberGreetingForFeet(feet){
+    if (feet <= 400){
+      return 'This one is on me!';
+  }
+  else if (feet>2000 && feet<2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else {
+    return 'No go.';
+  }
+
+};
+
+
+  // Write your code here!
+  function switchOnCharmFromTip(tip) {
+    // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return "Bye.";
+  }
+  
 }
